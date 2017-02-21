88b0fed6841ec128044bf1cf231b870960eb509e
Dirk Chang <dirk@kooiot.com>
2017-02-21 13:39:31 +0800

    for IOT User groups assignment

--- iot/iot/doctype/iot_user/iot_user.js
+++ iot/iot/doctype/iot_user/iot_user.js
@@ -2,7 +2,102 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('IOT User', {
+	onload: function(frm) {
+		if(has_common(user_roles, ["Administrator", "System Manager", "IOT Manager"]) && !frm.doc.__islocal) {
+			if(!frm.group_editor) {
+				var group_area = $('<div style="min-height: 300px">')
+					.appendTo(frm.fields_dict.groups_html.wrapper);
+				frm.group_editor = new frappe.GroupEditor(frm, group_area)
+			} else {
+				frm.group_editor.show();
+			}
+		}
+	},
 	refresh: function(frm) {
+        var doc = frm.doc;
 
+		if(doc.name===user && !doc.__unsaved
+			&& (doc.language || frappe.boot.user.language)
+			&& doc.language !== frappe.boot.user.language) {
+			msgprint(__("Refreshing..."));
+			window.location.reload();
+		}
+
+		frm.toggle_display(['group_settings'], false);
+
+		if(!doc.__islocal){
+			if(has_common(user_roles, ["Administrator", "System Manager", "IOT Manager"])) {
+				frm.toggle_display(['group_settings'], true);
+			}
+			frm.trigger('enabled');
+
+			frm.group_editor && frm.group_editor.refresh();
+		}
+
+		if (frappe.route_flags.unsaved===1){
+		    cur_frm.dirty();
+		}
+	},
+	enabled: function(frm) {
+		var doc = frm.doc;
+		if(!doc.__islocal && has_common(user_roles, ["Administrator", "System Manager", "IOT Manager"])) {
+			frm.toggle_display(['group_settings'], doc.enabled);
+			frm.set_df_property('enabled', 'read_only', 0);
+		}
+	}
+});
+
+
+frappe.GroupEditor = Class.extend({
+	init: function(frm, wrapper) {
+		this.wrapper = $('<div class="row group-block-list"></div>').appendTo(wrapper);
+		this.frm = frm;
+		this.load_groups(this.frm.doc.enterprise);
+	},
+	load_groups: function(enterprise) {
+		var me = this;
+		var wrapper = this.wrapper;
+		$(wrapper).html('<div class="help">' + __("Loading") + '...</div>')
+		return frappe.call({
+			method: 'iot.iot.doctype.iot_user.iot_user.get_all_groups',
+			args: {enterprise: enterprise},
+			callback: function(r) {
+				me.groups = r.message;
+				me.make();
+
+				// refresh call could've already happened
+				// when all role checkboxes weren't created
+				if(cur_frm.doc) {
+					cur_frm.group_editor.show();
+				}
+			}
+		});
+    },
+	make: function() {
+		var me = this;
+		me.groups.forEach(function(m) {
+			$(repl('<div class="col-sm-6"><div class="checkbox">\
+				<label><input type="checkbox" class="block-group-check" data-group="%(group)s">\
+				%(group)s</label></div></div>', {group: m})).appendTo(me.wrapper);
+		});
+		this.bind();
+	},
+	refresh: function() {
+		var me = this;
+		this.wrapper.find(".block-group-check").prop("checked", true);
+		$.each(this.frm.doc.group_assigned, function(i, d) {
+			me.wrapper.find(".block-group-check[data-group='"+ d.group +"']").prop("checked", false);
+		});
+	},
+	bind: function() {
+		this.wrapper.on("change", ".block-group-check", function() {
+			var group = $(this).attr('data-group');
+			if($(this).prop("checked")) {
+				// remove from group_assigned
+				me.frm.doc.group_assigned = $.map(me.frm.doc.group_assigned || [], function(d) { if(d.group != group){ return d } });
+			} else {
+				me.frm.add_child("group_assigned", {"group": group});
+			}
+		});
 	}
 });
--- iot/iot/doctype/iot_user/iot_user.py
+++ iot/iot/doctype/iot_user/iot_user.py
@@ -7,4 +7,28 @@ import frappe
 from frappe.model.document import Document
 
 class IOTUser(Document):
-	pass
+	__org_enterprise = None
+
+	def onload(self):
+		self.__org_enterprise = self.enterprise
+
+	def autoname(self):
+		"""set name as <Login Name>@<Enterprise Domain>"""
+		domain = frappe.db.get_value("IOT Enterprise", {"name": self.enterprise}, "domain")
+		self.name = self.login_name + domain
+
+	def validate(self):
+		# clear groups if Enterprise changed
+		if self.__org_enterprise != self.enterprise:
+			self.remove_all_groups()
+
+	def remove_all_groups(self):
+		self.set("group_assigned", list(set(d for d in self.get("group_assigned") if d.group == "Guest")))
+
+
+@frappe.whitelist()
+def get_all_groups(enterprise):
+	"""return all groups in specified enterprise"""
+	frappe.db.sql("""select name, grp_name
+			from `tabIOT Employee Group`
+			where parent = %(enterprise)s""", {"enterprise": enterprise}, as_dict=1)
