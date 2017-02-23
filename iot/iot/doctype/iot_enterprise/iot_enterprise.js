bb48a7d5ffbf460939a22f5b83ef0e0393989712
Dirk Chang <dirk@kooiot.com>
2017-02-23 16:01:04 +0800

    start to making pages

--- iot/iot/doctype/iot_enterprise/iot_enterprise.js
+++ iot/iot/doctype/iot_enterprise/iot_enterprise.js
@@ -2,6 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('IOT Enterprise', {
+	setup: function(frm) {
+		frm.fields_dict["user"].get_query = function(){
+			return {
+				filters: {"ignore_user_type": 1}
+			}
+		}
+	},
 	refresh: function(frm) {
 
 	}
