cf9d77a52dfae09b0726a75950e2ed1b66422020
Dirk Chang <dirk@kooiot.com>
2017-02-28 11:43:29 +0800

    set the igrnore_user_type only for User

--- iot/iot/doctype/iot_device_bunch/iot_device_bunch.js
+++ iot/iot/doctype/iot_device_bunch/iot_device_bunch.js
@@ -12,7 +12,7 @@ frappe.ui.form.on('IOT Device Bunch', {
 		};
 	},
 	refresh: function(frm) {
-		if (frm.field_dict["owner_type"].val() == 'User') {
+		if (frm.fields_dict["owner_type"].val() == 'User') {
 			frm.fields_dict["owner_id"].get_query = function () {
 				return {
 					filters: {"ignore_user_type": 1}
