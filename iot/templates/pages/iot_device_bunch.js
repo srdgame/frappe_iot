5ded61897a3dbf2e450f109954807c887adea7d0
Dirk Chang <dirk@kooiot.com>
2017-02-28 17:12:06 +0800

    ajax submit

--- iot/templates/pages/iot_device_bunch.js
+++ iot/templates/pages/iot_device_bunch.js
@@ -1,5 +1,6 @@
 frappe.ready(function() {
 	$(".form-enable-iot").submit(function() {
+		alert("AAAAAAAAAAAAAAAAAAAA");
   		$(this).ajaxSubmit({
 			success: function (responseText, statusText, xhr, $form) {
 				alert(responseText);
