abb7476e499f2a8b240a00a4e61c9f9ce1957172
Dirk Chang <dirk@kooiot.com>
2017-03-02 17:05:48 +0800

    top bars

--- iot/config/docs.py
+++ iot/config/docs.py
@@ -8,4 +8,11 @@ Configuration for docs
 # sub_heading = "Yes, you got that right the first time, everything"
 
 def get_context(context):
-	context.brand_html = "IOT"
+	context.brand_html = ('<img class="brand-logo" src="'+context.docs_base_url
+		+'/assets/img/frappe-bird-white.png"> Frappé Framework</img>')
+	context.top_bar_items = [
+		{"label": "IOT Enterprise", "url": context.docs_base_url + "/iot_enterprises", "right": 1},
+		{"label": "IOT Devices", "url": context.docs_base_url + "/iot_devices", "right": 1},
+		{"label": "IOT Account", "url": context.docs_base_url + "/iot_me", "right": 1},
+		{"label": "SymLink", "url": 'https://symid.com', "right": 1}
+	]
