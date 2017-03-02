9ccf3aed9e389f616dd3c9eb5aeebc5416563f0f
Dirk Chang <dirk@kooiot.com>
2017-03-02 17:27:27 +0800

    test title

--- iot/templates/pages/iot_enterprise.py
+++ iot/templates/pages/iot_enterprise.py
@@ -37,6 +37,7 @@ def get_context(context):
 
 	# context.parents = [{"label": _("IOT Enterprises"), "route": "/iot_enterprises"}]
 
+	context.title = _("IOT Enterprise")
 	context.doc = enterprise
 	"""
 	context.parents = [
