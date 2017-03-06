b3b158b4772649e6ed66eef96f18705ee151076d
Dirk Chang <dirk@kooiot.com>
2017-03-06 18:39:43 +0800

    adding default roles

--- iot/controllers/user_hooks.py
+++ iot/controllers/user_hooks.py
@@ -2,11 +2,8 @@
 # License: GNU General Public License v3. See license.txt
 
 from __future__ import unicode_literals
-import json
 import frappe
 from frappe import _
-from frappe.utils import flt
-from frappe.utils.user import is_website_user
 
 
 def after_insert(doc, method):
