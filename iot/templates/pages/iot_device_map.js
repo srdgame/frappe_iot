4a5f142f8cef5ff44b00ef10c31e0c970a5d816a
Dirk Chang <dirk@kooiot.com>
2017-03-06 12:49:16 +0800

    test device map

--- iot/templates/pages/iot_device_map.js
+++ iot/templates/pages/iot_device_map.js
@@ -34,7 +34,7 @@ frappe.ready(function() {
 					for (var dev in devices) {
 						pt = new BMap.Point(devices[dev].longitude, devices[dev].latitude);
 						var marker = new BMap.Marker(pt);
-						var sContent = 	"<h4 style='margin:0 0 5px 0;padding:0.2em 0'>" + devices[dev].name + "</h4>" +
+						var content = 	"<h4 style='margin:0 0 5px 0;padding:0.2em 0'>" + devices[dev].name + "</h4>" +
 										"<a href='/iot_devices/'" + devices[dev].sn + "'>Detials</a>";
 
 						addClickHandler(content, marker);
@@ -42,17 +42,6 @@ frappe.ready(function() {
 					}
 					//最简单的用法，生成一个marker数组，然后调用markerClusterer类即可。
 					var markerClusterer = new BMapLib.MarkerClusterer(map, {markers: markers});
-				}
-
-				var data_info = [[116.417854,39.921988,"地址：北京市东城区王府井大街88号乐天银泰百货八层"],
-					 [116.406605,39.921585,"地址：北京市东城区东华门大街"],
-					 [116.412222,39.912345,"地址：北京市东城区正义路甲5号"]
-					];
-				for(var i=0;i<data_info.length;i++) {
-					var marker = new BMap.Marker(new BMap.Point(data_info[i][0], data_info[i][1]));  // 创建标注
-					var content = data_info[i][2];
-					map.addOverlay(marker);               // 将标注添加到地图中
-					addClickHandler(content, marker);
 				}
 			} else {
 				if(r._server_messages)
