36eed8e09dc55349ecc2e62eeb51e3f2919abcb7
Dirk Chang <dirk@kooiot.com>
2017-03-10 12:06:25 +0800

    Hdb API

---
+++ iot/hdb.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, Dirk Chang and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+import json
+import requests
+from frappe import throw, msgprint, _
+from frappe.model.document import Document
+from iot.doctype.iot_device.iot_device import IOTDevice
+from iot.doctype.iot_hdb_settings.iot_hdb_settings import IOTHDBSettings
+from iot.doctype.iot_settings.iot_settings import IOTSettings
+from frappe.utils import cint
+
+
+@frappe.whitelist()
+def get_device_data(sn=None):
+	user = frappe.session.user
+	sn = sn or frappe.form_dict.get('sn')
+	doc = frappe.get_doc('IOT Device', sn)
+	doc.has_permission("read")
+	session = requests.session()
+	return session.get('127.0.0.1:8050/rtdb/'+doc.dev_name).json()
+
+
+def get_post_json_data():
+	if frappe.request.method != "POST":
+		throw(_("Request Method Must be POST!"))
+	ctype = frappe.get_request_header("Content-Type")
+	if "json" not in ctype.lower():
+		throw(_("Incorrect HTTP Content-Type found {0}").format(ctype))
+	if not frappe.form_dict.data:
+		throw(_("JSON Data not found!"))
+	return json.loads(frappe.form_dict.data)
+
+
+def fire_callback(cb_url, cb_data):
+	frappe.logger(__name__).debug("HDB Fire Callback with data:")
+	frappe.logger(__name__).debug(cb_data)
+	session = requests.session()
+	r = session.post(cb_url, json=cb_data)
+
+	if r.status_code != 200:
+		frappe.logger(__name__).error(r.text)
+	else:
+		frappe.logger(__name__).debug(r.text)
+
+
+@frappe.whitelist()
+def set_device_data(cmds=None):
+	cmds = cmds or get_post_json_data()
+	doc = frappe.get_doc('IOT Device', cmds.sn)
+
+	url = IOTHDBSettings.get_callback_url()
+	if url:
+		frappe.enqueue('iot.hdb_api.fire_callback', cb_url = url, cb_data = cmds)
+
+	return doc
+
+
+@frappe.whitelist(allow_guest=True)
+def ping():
+	form_data = frappe.form_dict
+	if frappe.request and frappe.request.method == "POST":
+		if form_data.data:
+			form_data = json.loads(form_data.data)
+		return form_data.get("text") or "No Text"
+	return 'pong'
+
+
