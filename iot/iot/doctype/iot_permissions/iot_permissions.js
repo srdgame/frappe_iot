f456085e3eb71121570fa3c609927f2ab36c989d
Dirk Chang <dirk.chang@symid.com>
2017-03-18 04:01:20 -0700

    Added user permission

--- iot/iot/doctype/iot_permissions/iot_permissions.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2016, Dirk Chang and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('IOT Permissions', {
-	refresh: function(frm) {
-
-	}
-});
