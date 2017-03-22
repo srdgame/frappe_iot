5ec7c2bafb779c4b2f694fb10ed9ea968dd09a0c
Dirk Chang <dirk@kooiot.com>
2017-03-22 20:20:33 +0800

    group

--- iot/iot/doctype/iot_device/iot_device.py
+++ iot/iot/doctype/iot_device/iot_device.py
@@ -117,12 +117,12 @@ def get_device_list(doctype, txt, filters, limit_start, limit_page_length=20, or
 			and bunch_code.owner_id = %(user)s
 			and bunch_code.code = device.bunch)
 			or (bunch_code.owner_type = "Cloud Company Group"
-			and bunch_code.owner_id in %groups
+			and bunch_code.owner_id in {3}
 			and bunch_code.code = device.bunch)
 			order by device.{0}
 			limit {1}, {2}
-		'''.format(order_by, limit_start, limit_page_length),
-			{'user' : frappe.session.user, 'groups': "('"+"','".join(groups)+"')"},
+		'''.format(order_by, limit_start, limit_page_length, "('"+"','".join(groups)+"')"),
+			{'user' : frappe.session.user},
 			as_dict=True,
 			update={'doctype' : 'IOT Device'})
 
