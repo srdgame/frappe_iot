9aa5e5d72754593df581a980b75b5e2f817be433
Dirk Chang <dirk@kooiot.com>
2017-03-22 18:30:13 +0800

    fitler the lists

--- iot/iot/doctype/iot_share_group/iot_share_group.js
+++ iot/iot/doctype/iot_share_group/iot_share_group.js
@@ -2,6 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('IOT Share Group', {
+	setup: function(frm) {
+		frm.fields_dict.devices.grid.get_field('device').get_query  = function(){
+			return {
+				filters: {"company": frm.doc.company}
+			};
+		};
+		frm.fields_dict.devices.grid.get_field('user').get_query  = function(){
+			return {
+				filters: {"ignore_user_type": 1}
+			};
+		};
+	},
 	refresh: function(frm) {
 
 	}
