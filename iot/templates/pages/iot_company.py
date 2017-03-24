d0504aa4190930dd0e6a988bd41bdf8d4f14d64f
Dirk Chang <dirk@kooiot.com>
2017-03-24 15:43:42 +0800

    redirect urlencode

--- iot/templates/pages/iot_company.py
+++ iot/templates/pages/iot_company.py
@@ -3,11 +3,11 @@
 
 from __future__ import unicode_literals
 import frappe
-import json
+from urllib import quote, urlencode
 from frappe import _
 from cloud.templates.pages.cloud_company import get_context as cloud_get_context
 
 def get_context(context):
 	name = frappe.form_dict.company or frappe.form_dict.name
-	frappe.local.flags.redirect_location = "/cloud_company?name=" + name + "&group_hack_url=iot_company_groups"
+	frappe.local.flags.redirect_location = "/cloud_company?name=" + urlencode(name) + "&group_hack_url=iot_company_groups"
 	raise frappe.Redirect
