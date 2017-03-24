d508bd9855317a8fadd02e6a3e64802b41b9e090
Dirk Chang <dirk@kooiot.com>
2017-03-24 18:00:33 +0800

    utf-8 encode

--- iot/templates/pages/iot_company.py
+++ iot/templates/pages/iot_company.py
@@ -4,10 +4,13 @@
 from __future__ import unicode_literals
 import frappe
 from urllib import quote, urlencode
-from frappe import _
-from cloud.templates.pages.cloud_company import get_context as cloud_get_context
+
 
 def get_context(context):
 	name = frappe.form_dict.company or frappe.form_dict.name
-	frappe.local.flags.redirect_location = "/cloud_company?name=" + urlencode(name) + "&group_hack_url=iot_company_groups"
+	#frappe.local.flags.redirect_location = "/cloud_company?name=" + quote(name.encode('utf-8')) + "&group_hack_url=iot_company_groups"
+	frappe.local.flags.redirect_location = "/cloud_company?" + urlencode({
+		"name": name.encode('utf-8'),
+		"group_hack_url": "iot_company_groups"
+	})
 	raise frappe.Redirect
