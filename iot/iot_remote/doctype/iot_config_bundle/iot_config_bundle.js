71197dd122047aa4de0fcdea1206a363e2c4424b
Dirk Chang <dirk@kooiot.com>
2017-05-08 18:37:27 +0800

    Config file bundle

---
+++ iot/iot_remote/doctype/iot_config_bundle/iot_config_bundle.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, Dirk Chang and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('IOT Config Bundle', {
+	refresh: function(frm) {
+
+	}
+});
