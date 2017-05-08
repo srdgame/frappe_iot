629079d9fb5369c347d6da433f42697f125f9b16
Dirk Chang <dirk@kooiot.com>
2017-05-08 17:20:58 +0800

    License permission and config device filter

--- iot/iot_remote/doctype/iot_device_config/iot_device_config.js
+++ iot/iot_remote/doctype/iot_device_config/iot_device_config.js
@@ -2,6 +2,22 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('IOT Device Config', {
+	setup: function(frm) {
+		frm.fields_dict["file"].get_query = function(doc){
+			return {
+				filters: {"group": doc.group}
+			};
+		};
+		frm.fields_dict["devices"].grid.get_field("device").get_query = function(doc, cdt, cdn) {
+			//var d  = locals[cdt][cdn];
+			return {
+				query: "iot.iot_remote.doctype.iot_device_config.iot_device_config.query_device",
+				filters: {
+					"group": doc.group
+				}
+			};
+		};
+	},
 	refresh: function(frm) {
 
 	}
