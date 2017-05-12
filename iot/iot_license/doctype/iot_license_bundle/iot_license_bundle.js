b2cf74e353603327cd3d263e8413c5692511b24e
Dirk Chang <dirk@kooiot.com>
2017-05-12 16:38:42 +0800

    IOT License Bundle source type/id

--- iot/iot_license/doctype/iot_license_bundle/iot_license_bundle.js
+++ iot/iot_license/doctype/iot_license_bundle/iot_license_bundle.js
@@ -2,6 +2,15 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('IOT License Bundle', {
+	setup: function(frm) {
+		frm.fields_dict["source_type"].get_query = function(){
+			return {
+				filters: {
+					"name": ["in","Stock Order,Cloud Company,User"]
+				}
+			}
+		};
+	},
 	refresh: function(frm) {
 
 	}
--- iot/iot_license/doctype/iot_license_bundle/iot_license_bundle.py
+++ iot/iot_license/doctype/iot_license_bundle/iot_license_bundle.py
@@ -4,7 +4,53 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import throw, _
 from frappe.model.document import Document
 
+
 class IOTLicenseBundle(Document):
-	pass
+	def validate(self):
+		for dev in self.devices:
+			if frappe.get_value("IOT License", dev.sn):
+				sid = frappe.get_value("IOT License", dev.sn, "source_id")
+				if sid != self.name:
+					throw(_("Device SN {0} is licensed by {1}").format(dev.sn, sid))
+
+	def after_insert(self):
+		for dev in self.devices:
+			doc = frappe.get_doc({
+				"doctype": "IOT License",
+				"sn": dev.sn,
+				"type": self.type,
+				"enabled": self.enabled,
+				"expire_date": self.expire_date,
+				"source_type": self.doctype,
+				"source_id": self.name
+			}).insert()
+
+	def on_update(self):
+		sn_list = [d.sn for d in self.devices]
+		for d in frappe.db.get_values("IOT License", {"source_id": self.name}, "sn"):
+			if d[0] not in sn_list:
+				frappe.delete_doc("IOT License", d[0])
+			else:
+				frappe.set_value("IOT License", d[0], "expire_date", self.expire_date)
+				frappe.set_value("IOT License", d[0], "type", self.type)
+				frappe.set_value("IOT License", d[0], "enabled", self.enabled)
+
+		for dev in self.devices:
+			if not frappe.get_value("IOT License", dev.sn):
+				doc = frappe.get_doc({
+					"doctype": "IOT License",
+					"sn": dev.sn,
+					"type": self.type,
+					"enabled": self.enabled,
+					"expire_date": self.expire_date,
+					"source_type": self.doctype,
+					"source_id": self.name
+				}).insert()
+
+	def on_trash(self):
+		for dev in self.devices:
+			frappe.delete_doc("IOT License", dev.sn)
+
