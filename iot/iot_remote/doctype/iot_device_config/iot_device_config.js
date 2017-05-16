fc409daefb0deb1c6fa7d67d026bbe65907e0aad
Dirk Chang <dirk@kooiot.com>
2017-05-16 11:32:09 +0800

    Query config file with config filter

--- iot/iot_remote/doctype/iot_device_config/iot_device_config.js
+++ iot/iot_remote/doctype/iot_device_config/iot_device_config.js
@@ -2,6 +2,15 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('IOT Device Config', {
+	setup: function(frm) {
+		frm.fields_dict['config_file'].get_query  = function(doc){
+			return {
+				filters: {
+					"config": doc.config
+				}
+			};
+		};
+	},
 	refresh: function(frm) {
 
 	}
