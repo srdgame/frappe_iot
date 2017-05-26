08f933f8c0856bd7c05902ccad6db564bcbd479c
e <3520845@qq.com>
2017-05-26 16:45:37 +0800

    Merge branch 'master' of github.com:srdgame/frappe_tieta
    # Conflicts:
    #	tieta/stock/doctype/stock_item/stock_item.json
    #	tieta/stock/doctype/stock_item_history/stock_item_history.json
    #	tieta/stock/doctype/stock_serial_no/stock_serial_no.json
    #	tieta/stock/doctype/stock_warehouse/stock_warehouse.json
    #	tieta/tieta/doctype/cell_station/cell_station.json
    #	tieta/tieta/doctype/cell_station/cell_station.py

--- iot/templates/pages/iot_device.js
+++ iot/templates/pages/iot_device.js
@@ -14,11 +14,28 @@ frappe.ready(function() {
         "dom": 'lfrtp',
         //"bInfo" : false,
         //"pagingType": "full_numbers" ,
+        "bStateSave": true,
+        "sPaginationType": "full_numbers",
+        "iDisplayLength" : 25,
         "ajax": {
             "url": rtvalueurl,
             //"url": "/api/method/tieta.tieta.doctype.cell_station.cell_station.list_station_map",
             "dataSrc": "message",
         },
+        "oLanguage": {
+            "sLengthMenu": "每页显示 _MENU_ 条记录",
+             "sZeroRecords": "抱歉， 没有找到",
+            "sInfo": "从 _START_ 到 _END_ /共 _TOTAL_ 条数据",
+            "sInfoEmpty": "没有数据",
+            "sInfoFiltered": "(从 _MAX_ 条数据中检索)",
+            "oPaginate": {
+                        "sFirst": "首页",
+                        "sPrevious": "前一页",
+                        "sNext": "后一页",
+                        "sLast": "尾页"
+                        },
+            "sZeroRecords": "没有检索到数据",
+            },
         "columns": [
             {"data": "NAME"},
             {"data": "DESC"},
