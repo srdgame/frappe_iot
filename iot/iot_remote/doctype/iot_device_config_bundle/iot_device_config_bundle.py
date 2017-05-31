fd90125ecbd7162db30e5c935550f57e751351a8
Dirk Chang <dirk@kooiot.com>
2017-05-31 15:04:44 +0800

    Doctype Rename

--- iot/iot_remote/doctype/iot_device_config_bundle/iot_device_config_bundle.py
+++ iot/iot_remote/doctype/iot_device_config_bundle/iot_device_config_bundle.py
@@ -4,7 +4,53 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import throw, _
 from frappe.model.document import Document
 
+
 class IOTDeviceConfigBundle(Document):
-	pass
+	def validate(self):
+		for dev in self.devices:
+			dev.device_name = frappe.get_value("IOT Device", dev.device, "dev_name")
+			if frappe.get_value("IOT Device Config", dev.device):
+				sid = frappe.get_value("IOT Device Config", dev.device, "source_id")
+				if sid != self.name:
+					throw(_("Device SN {0} is licensed by {1}").format(dev.device, sid))
+
+		self.config_name = frappe.get_value("IOT Config", self.config, "config_name")
+		self.config_version = frappe.get_value("IOT Config File", self.config_file, "version")
+	#
+	# def after_insert(self):
+	# 	for dev in self.devices:
+	# 		doc = frappe.get_doc({
+	# 			"doctype": "IOT Device Config",
+	# 			"device": dev.device,
+	# 			"config": self.config,
+	# 			"config_file": self.config_file,
+	# 			"source_type": self.doctype,
+	# 			"source_id": self.name
+	# 		}).insert()
+
+	def on_update(self):
+		dev_list = [d.device for d in self.devices]
+		for d in frappe.db.get_values("IOT Device Config", {"source_id": self.name}, "device"):
+			if d[0] not in dev_list:
+				frappe.delete_doc("IOT Device Config", d[0])
+			else:
+				frappe.set_value("IOT Device Config", d[0], "config", self.config)
+				frappe.set_value("IOT Device Config", d[0], "config_file", self.config_file)
+
+		for dev in self.devices:
+			if not frappe.get_value("IOT Device Config", dev.device):
+				doc = frappe.get_doc({
+					"doctype": "IOT Device Config",
+					"device": dev.device,
+					"config": self.config,
+					"config_file": self.config_file,
+					"source_type": self.doctype,
+					"source_id": self.name
+				}).insert()
+
+	def on_trash(self):
+		for dev in self.devices:
+			frappe.delete_doc("IOT Device Config", dev.device)
