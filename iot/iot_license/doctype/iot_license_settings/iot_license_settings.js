f14436880da108aae95ac90c87861f1739b5c74f
Dirk Chang <dirk@kooiot.com>
2017-06-07 17:09:46 +0800

    License Module moved to Make App

--- iot/iot_license/doctype/iot_license_settings/iot_license_settings.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2017, Dirk Chang and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('IOT License Settings', {
-	refresh: function(frm) {
-
-	}
-});
