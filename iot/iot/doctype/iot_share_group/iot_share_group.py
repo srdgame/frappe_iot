5644ba437a74c6347633e9fac8373762fdfdaf54
Dirk Chang <dirk@kooiot.com>
2017-07-06 16:42:04 +0800

    interfaces for add_users and remove_users in group

--- iot/iot/doctype/iot_share_group/iot_share_group.py
+++ iot/iot/doctype/iot_share_group/iot_share_group.py
@@ -25,7 +25,7 @@ class IOTShareGroup(Document):
 
 	def append_devices(self, *devices):
 		"""Add groups to user"""
-		current_devices = [d.group for d in self.get("devices")]
+		current_devices = [d.device for d in self.get("devices")]
 		for device in devices:
 			if device in current_devices:
 				continue
@@ -39,7 +39,7 @@ class IOTShareGroup(Document):
 		self.save()
 
 	def remove_devices(self, *devices):
-		existing_devices = dict((d.group, d) for d in self.get("devices"))
+		existing_devices = dict((d.device, d) for d in self.get("devices"))
 		for device in devices:
 			if device in existing_devices:
 				self.get("devices").remove(existing_devices[device])
@@ -50,7 +50,7 @@ class IOTShareGroup(Document):
 		from cloud.cloud.doctype.cloud_company.cloud_company import list_user_companies
 
 		"""Add groups to user"""
-		current_users = [d.group for d in self.get("users")]
+		current_users = [d.user for d in self.get("users")]
 		for user in users:
 			if user in current_users:
 				continue
@@ -64,7 +64,7 @@ class IOTShareGroup(Document):
 		self.save()
 
 	def remove_users(self, *users):
-		existing_users = dict((d.group, d) for d in self.get("users"))
+		existing_users = dict((d.user, d) for d in self.get("users"))
 		for user in users:
 			if user in existing_users:
 				self.get("users").remove(existing_users[user])
