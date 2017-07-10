80472044e8a219e36d0cca615ab1d8a10b5aaa10
Dirk Chang <dirk@kooiot.com>
2017-07-10 16:40:51 +0800

    Create company database with retry

--- iot/controllers/cloud_company_hooks.py
+++ iot/controllers/cloud_company_hooks.py
@@ -5,19 +5,33 @@
 from __future__ import unicode_literals
 import frappe
 import requests
+import time
 from frappe import _, throw
 from iot.iot.doctype.iot_hdb_settings.iot_hdb_settings import IOTHDBSettings
 
 def after_insert(doc, method):
+	frappe.enqueue('iot.controllers.cloud_company_hooks.create_influxdb', db_name=doc.domain)
+
+
+def create_influxdb(db_name, sleep=None):
+	if sleep:
+		time.sleep(sleep)
+
 	inf_server = IOTHDBSettings.get_influxdb_server()
 	if not inf_server:
 		frappe.logger(__name__).error("InfluxDB Configuration missing in IOTHDBSettings")
 		return
 
-	r = requests.session().get(inf_server + "/query", params={ "q": ('''CREATE DATABASE "{0}"''').format(doc.domain)	})
+	try:
+		r = requests.session().get(inf_server + "/query", params={"q": ('''CREATE DATABASE "{0}"''').format(db_name)}, timeout=1)
 
-	if r.status_code != 200:
-		frappe.logger(__name__).error(r.text)
-		throw(r.text)
-	else:
-		frappe.logger(__name__).debug(r.text)
+		if r.status_code != 200:
+			frappe.logger(__name__).error(r.text)
+			frappe.enqueue('iot.controllers.cloud_company_hooks.create_influxdb', db_name=db_name, sleep=60)
+			throw(r.text)
+		else:
+			frappe.logger(__name__).debug(r.text)
+	except Exception as ex:
+		frappe.logger(__name__).error(ex.message)
+		frappe.enqueue('iot.controllers.cloud_company_hooks.create_influxdb', db_name=db_name, sleep=60)
+		throw(ex.message)
