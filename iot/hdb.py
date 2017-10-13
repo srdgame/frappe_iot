4368bdb223c8a287369bf1fc0c8bb7ea22d574a1
viccom <3520845@qq.com>
2017-10-13 16:50:20 +0800

    m

--- iot/hdb.py
+++ iot/hdb.py
@@ -15,6 +15,16 @@ from iot.doctype.iot_hdb_settings.iot_hdb_settings import IOTHDBSettings
 from hdb_api import valid_auth_code
 
 
+UTC_FORMAT1 = "%Y-%m-%dT%H:%M:%S.%fZ"
+UTC_FORMAT2 = "%Y-%m-%dT%H:%M:%SZ"
+def utc2local(utc_st):
+    now_stamp = time.time()
+    local_time = datetime.datetime.fromtimestamp(now_stamp)
+    utc_time = datetime.datetime.utcfromtimestamp(now_stamp)
+    offset = local_time - utc_time
+    local_st = utc_st + offset
+    return local_st
+
 @frappe.whitelist()
 def redis_status():
 	from iot.doctype.iot_hdb_settings.iot_hdb_settings import get_redis_status
@@ -146,6 +156,53 @@ def iot_device_his_data(sn=None, vsn=None, fields=None, condition=None):
 
 	return r.text
 
+
+
+@frappe.whitelist()
+def taghisdata(sn=None, vsn=None, fields=None, condition=None):
+	vsn = vsn or sn
+	fields = fields or "*"
+	doc = frappe.get_doc('IOT Device', sn)
+	doc.has_permission("read")
+
+	inf_server = IOTHDBSettings.get_influxdb_server()
+	if not inf_server:
+		frappe.logger(__name__).error("InfluxDB Configuration missing in IOTHDBSettings")
+		return 500
+	query = 'SELECT ' + fields + ' FROM "' + vsn + '"'
+	if condition:
+		query = query + " WHERE " + condition
+	else:
+		query = query + " LIMIT 1000"
+
+	domain = frappe.get_value("Cloud Company", doc.company, "domain")
+	r = requests.session().get(inf_server + "/query", params={"q": query, "db": domain}, timeout=10)
+	if r.status_code == 200:
+		try:
+			res = r.json()["results"][0]['series'][0]['values']
+			taghis = []
+			for i in range(0, len(res)):
+				hisvalue = {}
+				#print('*********', res[i][0])
+				try:
+					utc_time = datetime.datetime.strptime(res[i][0], UTC_FORMAT1)
+				except Exception as err:
+					pass
+				try:
+					utc_time = datetime.datetime.strptime(res[i][0], UTC_FORMAT2)
+				except Exception as err:
+					pass
+					local_time = str(convert_utc_to_user_timezone(utc_time).replace(tzinfo=None))
+				#print('#######', local_time)
+				if res[i][2] == '1':
+					hisvalue = {'name': res[i][1], 'value': res[i][4], 'time': local_time, 'quality': 0}
+				elif res[i][2] == '2':
+					hisvalue = {'name': res[i][1], 'value': res[i][3], 'time': local_time, 'quality': 0}
+				taghis.append(hisvalue)
+			#print(taghis)
+			return taghis
+		except Exception as err:
+			return r.json()
 
 @frappe.whitelist()
 def iot_device_tree(sn=None):
