ab035a0afc53a316ec5e985ff0718c3d38ce5c03
Dirk Chang <dirk@kooiot.com>
2017-12-06 16:23:53 +0800

    define the after_insert handler inside document instead of hooks

--- iot/iot/doctype/iot_device_error/iot_device_error.py
+++ iot/iot/doctype/iot_device_error/iot_device_error.py
@@ -7,8 +7,13 @@ import frappe
 from frappe import _
 from frappe.model.document import Document
 from frappe.utils.data import format_datetime
+from iot.iot.doctype.iot_device_error_rule.iot_device_error_rule import wechat_notify_check
+
 
 class IOTDeviceError(Document):
+	def after_insert(self):
+		wechat_notify_check(self)
+
 	def on_submit(self):
 		self.wechat_msg_send()
 
