925305ba323ed9d640e7f3ffa958b565a67d0542
Dirk Chang <dirk@kooiot.com>
2018-03-20 11:11:49 +0800

    IOT Batch Script tasks

--- iot/iot_remote/doctype/iot_batch_task/iot_batch_task.py
+++ iot/iot_remote/doctype/iot_batch_task/iot_batch_task.py
@@ -4,6 +4,7 @@
 
 from __future__ import unicode_literals
 import frappe
+import time
 from frappe import throw, _
 from frappe.model.document import Document
 
@@ -13,7 +14,7 @@ class IOTBatchTask(Document):
 		self.owner_id = frappe.session.user
 
 	def on_submit(self):
-		frappe.enqueue_doc('IOT Batch Task', self.name, 'run_task')
+		frappe.enqueue_doc('IOT Batch Task', self.name, 'run_task', timeout=self.timeout + 60)
 
 	def run_task(self):
 		if self.docstatus != 1:
@@ -22,14 +23,19 @@ class IOTBatchTask(Document):
 			return
 
 		device_list = self.get("device_list")
+		timeout = self.timeout + 60
 
 		for device in device_list:
-			frappe.enqueue_doc('IOT Batch TaskDevice', device.name, 'run_batch_script')
+			frappe.enqueue_doc('IOT Batch TaskDevice', device.name, 'run_batch_script', timeout=timeout)
 
 		self.set("status", "Running")
 		self.save()
 
+		time.sleep(self.timeout)
+		self.update_status()
+
 	def update_status(self):
+		self.reload()
 		device_list = self.get("device_list")
 		count = 0
 		for device in device_list:
@@ -38,3 +44,4 @@ class IOTBatchTask(Document):
 				return
 
 		self.set("status", "Finished")
+		self.save()
--- iot/iot_remote/doctype/iot_batch_taskdevice/iot_batch_taskdevice.py
+++ iot/iot_remote/doctype/iot_batch_taskdevice/iot_batch_taskdevice.py
@@ -4,6 +4,8 @@
 
 from __future__ import unicode_literals
 import frappe
+import time
+from frappe import throw, _
 from frappe.model.document import Document
 
 class IOTBatchTaskDevice(Document):
@@ -12,16 +14,23 @@ class IOTBatchTaskDevice(Document):
 		if self.status != 'New':
 			return
 
+		timeout = time.time() + task.timeout
+
 		frappe.logger(__name__).info("Run batch script {0} on device {1}".format(task.name, self.device))
 
 		try:
-			from iot.device_api import send_action
+			from iot.device_api import send_action, get_action_result
 			task.set("status", "Running")
 			id = send_action("sys", action="batch_script", device=self.device, data=task.batch_script)
 			while True:
-				a = 1
-			self.set("status", "Finished")
-			self.set("info", "Script run completed")
+				time.sleep(5)
+				if time.time() >= timeout:
+					throw("Timeout!!!")
+				result = get_action_result(id)
+				print(result)
+				if result.get('result') == True or result.get('result') == 'True':
+					self.set("status", "Finished")
+					self.set("info", "Script run completed")
 		except Exception, e:
 			frappe.logger(__name__).error(_("Run batch script {0} on {1} failed {1}").format(task.name, self.device, e.message))
 			self.set("status", 'Error')
