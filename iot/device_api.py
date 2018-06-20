7cec50e35fcc50161c008818ba811ef1d703552e
Dirk Chang <dirk@kooiot.com>
2018-06-20 14:29:02 +0800

    using user auth code

--- iot/device_api.py
+++ iot/device_api.py
@@ -12,15 +12,23 @@ from iot.doctype.iot_device.iot_device import IOTDevice
 from iot.doctype.iot_hdb_settings.iot_hdb_settings import IOTHDBSettings
 
 
-def valid_auth_code(auth_code=None):
-	auth_code = auth_code or frappe.get_request_header("HDB-AuthorizationCode")
-	if not auth_code:
-		throw(_("HDB-AuthorizationCode is required in HTTP Header!"))
-	frappe.logger(__name__).debug(_("HDB-AuthorizationCode as {0}").format(auth_code))
+def valid_auth_code():
+	auth_code = frappe.get_request_header("HDB-AuthorizationCode")
+	user = None
+	if auth_code:
+		frappe.logger(__name__).debug(_("HDB-AuthorizationCode as {0}").format(auth_code))
 
-	user = IOTHDBSettings.get_on_behalf(auth_code)
+		user = IOTHDBSettings.get_on_behalf(auth_code)
+	else:
+		auth_code = frappe.get_request_header("AuthorizationCode")
+		if auth_code:
+			user = frappe.get_value("IOT User Api", {"authorization_code": auth_code}, "user")
+		else:
+			throw(_("Authorization Code/Login is required!"))
+
 	if not user:
 		throw(_("Authorization Code is incorrect!"))
+
 	# form dict keeping
 	form_dict = frappe.local.form_dict
 	frappe.set_user(user)
