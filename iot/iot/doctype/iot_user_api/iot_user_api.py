8f51432a092aee91c00bb68f18fbd2261a6e448c
Dirk Chang <dirk@kooiot.com>
2018-09-17 19:41:30 +0800

    Using interface instead of saving information in redis db(14)

--- iot/iot/doctype/iot_user_api/iot_user_api.py
+++ iot/iot/doctype/iot_user_api/iot_user_api.py
@@ -11,24 +11,4 @@ from cloud.cloud.doctype.cloud_company.cloud_company import list_admin_companies
 
 
 class IOTUserApi(Document):
-	def on_update(self):
-		self.enable_statistics(1)
-
-	def on_trash(self):
-		self.enable_statistics(0)
-
-	def enable_statistics(self, enable):
-		companies = list_admin_companies(self.user)
-		if len(companies) == 0:
-			return
-
-		client = redis.Redis.from_url(IOTHDBSettings.get_redis_server() + "/14")
-
-		for company in companies:
-			doc = frappe.get_doc('Cloud Company', company)
-			client.hmset(doc.domain, {
-				'company': doc.name,
-				'auth_code': self.authorization_code,
-				'database': doc.domain,
-				'enable': enable,
-			})
+	pass
