27a9b0192bffa5c7067c1d49de36b0fda6ffbe72
Dirk Chang <dirk@kooiot.com>
2018-09-18 17:09:28 +0800

    Work on IOT Hub

---
+++ iot/iot_hub/doctype/iot_user_application/iot_user_application.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, Dirk Chang and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('IOT User Application', {
+	refresh: function(frm) {
+
+	}
+});
