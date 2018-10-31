afa7c6530fe9c892b3f51fd3dc87980aa57b5b83
Dirk Chang <dirk@kooiot.com>
2018-10-31 15:21:13 +0800

    correct the scheduler_events in hooks

--- iot/hooks.py
+++ iot/hooks.py
@@ -176,9 +176,9 @@ doc_events = {
 # 	]
 # }
 scheduler_events = {
-	"all": {
+	"all": [
 		"iot.iot.doctype.iot_batch_task.iot_batch_task.check_all_task_status"
-	},
+	],
 	"daily": [
 		"iot.iot.doctype.iot_device_activity.iot_device_activity.clear_device_activities",
 		"iot.iot.doctype.iot_device_event.iot_device_event.clear_device_events"
