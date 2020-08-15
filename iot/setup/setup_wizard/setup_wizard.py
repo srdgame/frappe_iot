98b2381c945a54cd582119d0ae54c2c65a4efc45
Dirk Chang <dirk@kooiot.com>
2020-08-15 16:50:15 +0800

    Fixed the single document save

--- iot/setup/setup_wizard/setup_wizard.py
+++ iot/setup/setup_wizard/setup_wizard.py
@@ -28,17 +28,18 @@ def get_setup_stages(args=None):
 
 
 def setup_hdb_settings(args):
-	frappe.logger(__name__).error("IOT Setup Wizard Args:{0}".format(json.dumps(args)))
 	if frappe.get_value('User', args.hdb_user) is None:
 		hdb_user = dict(doctype='User', email=args.hdb_user, first_name='HDB User', send_welcome_email=0, enabled=1)
 		frappe.get_doc(hdb_user).insert(ignore_permissions=True)
 
-	frappe.set_value("IOT HDB Settings", "IOT HDB Settings", "authorization_code", args.hdb_authorization_code)
-	frappe.set_value("IOT HDB Settings", "IOT HDB Settings", "on_behalf", args.hdb_user)
-	frappe.set_value("IOT HDB Settings", "IOT HDB Settings", "redis_server", args.hdb_redis_server)
-	frappe.set_value("IOT HDB Settings", "IOT HDB Settings", "influxdb_server", args.hdb_influxdb_server)
-	frappe.set_value("IOT HDB Settings", "IOT HDB Settings", "mqtt_root_password", args.hdb_mqtt_root_password)
-	frappe.set_value("IOT HDB Settings", "IOT HDB Settings", "mqtt_device_password_sid", args.hdb_mqtt_device_password_sid)
+	settings = frappe.get_doc("IOT HDB Settings")
+	settings.set("authorization_code", args.hdb_authorization_code)
+	settings.set("on_behalf", args.hdb_user)
+	settings.set("redis_server", args.hdb_redis_server)
+	settings.set("influxdb_server", args.hdb_influxdb_server)
+	settings.set("mqtt_root_password", args.hdb_mqtt_root_password)
+	settings.set("mqtt_device_password_sid", args.hdb_mqtt_device_password_sid)
+	settings.save()
 
 
 # Only for programmatical use
